54cb602ac089deae30b50db2ee2f180187c1a35b
luthor628 <abdulrahmanabdulaziz628@gmail.com>
2023-02-28 13:10:47 +0300

    first commit

---
+++ djangoregistrationRisley/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+
+def index_page(request):
+    return render(request, "index.html")
+
+
+def login_page(request):
+    return render(request, "login.html")
+
+
+def signup_page(request):
+    return render(request, "signup.html")
+
+
+def edit_Page(request):
+    return render(request, "edit.html")
