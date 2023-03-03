dc3ad8553b2562a2a3f084e3c5b87d245c876f48
luthor628 <abdulrahmanabdulaziz628@gmail.com>
2023-03-03 11:59:31 +0300

    django

--- djangoregistrationRisley/urls.py
+++ djangoregistrationRisley/urls.py
@@ -19,9 +19,12 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index_page, name='index'),
+    path('', views.index_page, name='index'),
     path('login/', views.login_page, name='login'),
     path('signup/', views.signup_page, name='signup'),
-    path('edit/', views.edit_Page, name='editpage')
+    path('edit/', views.edit_Page, name='editpage'),
+    path('insert', views.insertData, name='insertdata'),
+    path('delete/<id>', views.deleteData, name='deletedata'),
+    path('update/<id>', views.updateData, name='updatedata'),
 
 ]
--- djangoregistrationRisley/views.py
+++ djangoregistrationRisley/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .models import Student
 
 
 def index_page(request):
-    return render(request, "index.html")
+    data = Student.objects.all()
+    context = {'data': data}
+    return render(request, "index.html", context)
 
 
 def login_page(request):
@@ -17,3 +20,51 @@ def signup_page(request):
 
 def edit_Page(request):
     return render(request, "edit.html")
+
+
+def insertData(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+        phone = request.POST.get('phone')
+        country = request.POST.get('country')
+
+        query = Student(name=name, email=email, age=age, gender=gender, phone=phone, country=country)
+        query.save()
+        return redirect("/")
+
+        return render(request, "/")
+
+
+def deleteData(request, id):
+    d = Student.objects.get(id=id)
+    d.delete()
+    return redirect("/")
+    return render(request, "index.html")
+
+
+def updateData(request, id):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        age = request.POST.get('age')
+        phone = request.POST.get('phone')
+        country = request.POST.get('country')
+        gender = request.POST.get('gender')
+
+        update_info = Student.objects.get(id=id)
+        update_info.name = name
+        update_info.email = email
+        update_info.age = age
+        update_info.phone = phone
+        update_info.country = country
+        update_info.gender = gender
+
+        update_info.save()
+        return redirect("/")
+
+    d = Student.objects.get(id=id)
+    context = {"d": d}
+    return render(request, "edit.html", context)
